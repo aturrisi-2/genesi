fcdad2899dc2584a73f51575b996f6e1a8c2a67f
aturrisi-2 <idappleturrisi@gmail.com>
2026-02-23 16:26:19 +0100

    Fix monkeypatch to avoid argument shifting and add clamping for time values

--- lab/test_icloud_v4.py
+++ lab/test_icloud_v4.py
@@ -17,26 +17,55 @@ original_date = datetime.date
 
 class PatchedDatetime(original_datetime):
     def __new__(cls, *args, **kwargs):
-        if args and len(args) >= 1:
-            year = args[0]
+        new_args = list(args)
+        if new_args:
+            year = new_args[0]
+            # Caso 1: Anno impaccato YYYYMMDD
             if year > 9999:
-                # Ripara YYYYMMDD -> YYYY, MM, DD
                 y = year // 10000
                 m = (year % 10000) // 100
                 d = year % 100
-                args = (y, m, d) + args[1:]
-        return original_datetime.__new__(cls, *args, **kwargs)
+                # Se abbiamo altri argomenti, dobbiamo stare attenti a non shiftarli male
+                # Se la chiamata era datetime(20191203, 1, 1), pyicloud probabilmente voleva 2019-12-03
+                # e i successivi 1, 1 erano fittizi o duplicati
+                if len(new_args) > 1:
+                    # Sostituiamo i primi 3 con y, m, d se possibile
+                    new_args[0] = y
+                    if len(new_args) > 1: new_args[1] = m
+                    if len(new_args) > 2: new_args[2] = d
+                else:
+                    new_args = [y, m, d]
+            
+            # Caso 2: Hour/Minute/Second fuori range (clamping)
+            if len(new_args) > 3: # Hour
+                if new_args[3] < 0 or new_args[3] > 23:
+                    print(f"⚠️  Fixing invalid hour: {new_args[3]} -> 0")
+                    new_args[3] = 0
+            if len(new_args) > 4: # Minute
+                if new_args[4] < 0 or new_args[4] > 59:
+                    new_args[4] = 0
+            if len(new_args) > 5: # Second
+                if new_args[5] < 0 or new_args[5] > 59:
+                    new_args[5] = 0
+
+        return original_datetime.__new__(cls, *tuple(new_args), **kwargs)
 
 class PatchedDate(original_date):
     def __new__(cls, *args, **kwargs):
-        if args and len(args) >= 1:
-            year = args[0]
+        new_args = list(args)
+        if new_args:
+            year = new_args[0]
             if year > 9999:
                 y = year // 10000
                 m = (year % 10000) // 100
                 d = year % 100
-                args = (y, m, d) + args[1:]
-        return original_date.__new__(cls, *args, **kwargs)
+                if len(new_args) > 1:
+                    new_args[0] = y
+                    if len(new_args) > 1: new_args[1] = m
+                    if len(new_args) > 2: new_args[2] = d
+                else:
+                    new_args = [y, m, d]
+        return original_date.__new__(cls, *tuple(new_args), **kwargs)
 
 # Applica la patch globale
 datetime.datetime = PatchedDatetime
